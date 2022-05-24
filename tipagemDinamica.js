ac3f1931d0ee296f32a58586fd3ae7e987aeee22
Yago <54825415+yagoSP@users.noreply.github.com>
2022-05-24 15:16:04 -0300

    Create tipagemDinamica.js

---
+++ tipagemDinamica.js
@@ -0,0 +1,9 @@
+nome = 5;
+console.log('O valor da variável nome é ', nome);
+console.log('O tipo da variável nome é ', typeof  nome);
+
+nome = 'chiquinha';
+console.log('O valor da variável nome agora é ', nome);
+console.log('E agora o tipo dela mudou para ', typeof nome);
+
+console.log('Os dois valores foram atrubuidos à mesma variável!');
