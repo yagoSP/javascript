b873244fbc4b1261674e122e6038470c5f844ac8
Yago <54825415+yagoSP@users.noreply.github.com>
2022-05-24 14:45:30 -0300

    Create var.js

---
+++ var.js
@@ -0,0 +1,11 @@
+var idade = 16;
+var nome = 'Yago Silva Pereira';
+var estado = 'sp';
+var saldo = -56;
+
+console.log(idade);
+console.log(nome);
+console.log(estado);
+console.log(saldo);
+
+//Primeiros passos em JS 24/05/2022 14:45
