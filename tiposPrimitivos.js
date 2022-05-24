fac9a0b1410cb2e720c05d9c77a0d1fffc869c4a
Yago <54825415+yagoSP@users.noreply.github.com>
2022-05-24 14:58:34 -0300

    Create tiposPrimitivos.js

---
+++ tiposPrimitivos.js
@@ -0,0 +1,5 @@
+var nickname = 'Chorelis_'; // string literal
+var idade = 20; // number literal (pode ser float)
+var aprovado = true // boolean (true, false)
+var qualquer; // undefined 
+var corSelecionada = null; // redefinir um valor
